f8c4762010ca7634cee03cc9b74cfc56ab97cb67
guohjjack <guohjjack@sina.com>
2016-07-10 14:28:11 +0800

    wyyx

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(function(){
+	var   bj31=document.querySelector(".bj31");
+	var   bj32=document.querySelector(".bj32");
+	var   val=document.querySelector(".val")
+	var   beautful=document.querySelector(".beautful")
+	$('#dowebok').fullpage({
+		sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90','#fff','#fff','#fff','#fff','#fff','#fff'],
+		'navigation': true,
+     // afterLoad(afterLoad,index){
+     //       if (index==2) {
+     //      beautful.className+=" active"
+     //      val.className+=" active"
+
+     //       }
+
+     // }
+
+
+	});
+});
